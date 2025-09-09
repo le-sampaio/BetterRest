02069f02eec90b3e9cf24bbacf4362fb7a368d9d
le-sampaio <leticia.souzza14@gmail.com>
2025-09-09 11:39:35 -0300

    new commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Leticia Sampaio on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
